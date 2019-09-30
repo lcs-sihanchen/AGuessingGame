b5c56080c0dea75a735ce7054b2da61755b8c127
Chen Sihan <schen@lcs.on.ca>
2019-09-30 11:11:03 -0400

    Initial Commit

---
+++ AGuessingGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AGuessingGame
+//
+//  Created by Chen, Sihan on 2019-09-30.
+//  Copyright © 2019 Chen, Sihan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
